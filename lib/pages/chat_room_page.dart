b88c67e91ff3862288c28c4d0ea99aea4a758933
zahid2873 <mahdi.zhm13@gmail.com>
2023-04-29 13:07:19 +0600

    route commit

---
+++ lib/pages/chat_room_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatRoomPage extends StatefulWidget {
+  static const String routeName = '/chat_room';
+  const ChatRoomPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChatRoomPage> createState() => _ChatRoomPageState();
+}
+
+class _ChatRoomPageState extends State<ChatRoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
