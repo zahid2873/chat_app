7768a6a5b1672897dfe218ad2d2a56898c179198
zahid2873 <mahdi.zhm13@gmail.com>
2023-05-01 22:31:19 +0600

    usermodel commit

---
+++ lib/models/user_model.dart
@@ -0,0 +1,40 @@
+class UserModel {
+  String uid;
+  String? name;
+  String email;
+  String? mobile;
+  String? image;
+  bool available;
+  String? deviceToken;
+
+  UserModel(
+      {required this.uid,
+        this.name,
+        required this.email,
+        this.mobile,
+        this.image,
+        this.available = true,
+        this.deviceToken});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic> {
+      'uid' : uid,
+      'name' : name,
+      'mobile' : mobile,
+      'email' : email,
+      'image' : image,
+      'deviceToken' : deviceToken,
+      'available' : available,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) => UserModel(
+    uid: map['uid'],
+    name: map['name'],
+    mobile: map['mobile'],
+    email: map['email'],
+    image: map['image'],
+    deviceToken: map['deviceToken'],
+    available: map['available'],
+  );
+}
