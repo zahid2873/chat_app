c5b21ddb34dcc3ca0f866cc7deddb44fff92785a
zahid2873 <mahdi.zhm13@gmail.com>
2023-05-07 23:28:03 +0600

    drawer header commit

--- lib/widget/main_drawer.dart
+++ lib/widget/main_drawer.dart
@@ -1,9 +1,13 @@
 import 'package:chat_app/auth/firebase_auth.dart';
 import 'package:chat_app/pages/chat_room_page.dart';
 import 'package:chat_app/pages/profile_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/user_model.dart';
 import '../pages/launcher_page.dart';
+import '../providers/user_provider.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({Key? key}) : super(key: key);
@@ -13,45 +17,74 @@ class MainDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          // Container(
-          //   height: 200,
-          //   color: Colors.blue,
-          // ),
-          // DrawerHeader(padding: EdgeInsets.zero, child: UserAccountsDrawerHeader(
-          //   accountName: const Text(userModel.),
-          //   accountEmail: Text("billgates96@gmail.com"),
-          //   currentAccountPicture: Container(
-          //     height: 100,
-          //     width: 100,
-          //     child: ClipRRect(
-          //         borderRadius: BorderRadius.circular(60),
-          //         child: Image.network('http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQi4a8NzG1ocCbgUUZxxDLocQwDQvhod4gHC3aRRg3juK0LDsZHECn7AwMJq8WUmPFLai9IJhY5YWNLRys',fit: BoxFit.cover,)),
-          //   ),
-          // )),
+// Container(
+//   height: 200,
+//   color: Colors.blue,
+// ),
+          Consumer<UserProvider>(
+          builder: (context, provider,_)=>
+              StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: provider.getUserById(AuthService.user!.uid),
+                  builder: (context,snapshot){
+                  if(snapshot.hasData){
+                  final userModel = UserModel.fromMap(snapshot.data!.data()!);
+                    return DrawerHeader(padding: EdgeInsets.zero, child: UserAccountsDrawerHeader(
+                      accountName:  Text(userModel.name ?? "No Display Name"),
+                      accountEmail: Text(userModel.email ),
+                      currentAccountPicture: Container(
+                        height: 100,
+                        width: 100,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(60),
+                            child: userModel.image == null?
+                            Image.asset("images/person.png",width: 100, height: 100,fit: BoxFit.cover,):
+                            Image.network(userModel.image!,width: 100, height: 100,fit: BoxFit.cover,),
+                      ),
+                    ))
+                    );
+                  }
+                     if(snapshot.hasError){
+                          return const Text("Failed To fetch data");
+                      }
 
-          ListTile(
-            onTap: (){
-              Navigator.pushReplacementNamed(context, ChatRoomPage.routeName);
-            },
-            leading: const Icon(Icons.chat),
-            title: const Text("Chat Room"),
-          ),
-          ListTile(
-            onTap: (){
-              Navigator.pushReplacementNamed(context, ProfilePage.routeName);
-            },
-            leading: const Icon(Icons.person),
-            title: const Text("Profile page"),
-          ),
-          ListTile(
-            onTap: (){
-              AuthService.logOut().then((value) => Navigator.pushReplacementNamed(context, LauncherPage.routeName));
-            },
-            leading: Icon(Icons.logout),
-            title: Text("Log Out"),
-          )
+                        return const  Center(
+                            child: CircularProgressIndicator(),
+                        );
+                    },
+                ),
+              ),
+                      Card(
+                        child: ListTile(
+                          onTap: (){
+                            Navigator.pushReplacementNamed(context, ChatRoomPage.routeName);
+                          },
+                          leading: const Icon(Icons.chat),
+                          title: const Text("Chat Room"),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          onTap: (){
+                            Navigator.pushReplacementNamed(context, ProfilePage.routeName);
+                          },
+                          leading: const Icon(Icons.person),
+                          title: const Text("Profile page"),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          onTap: (){
+                            AuthService.logOut().then((value) => Navigator.pushReplacementNamed(context, LauncherPage.routeName));
+                          },
+                          leading: Icon(Icons.logout),
+                          title: Text("Log Out"),
+                        ),
+                      )
         ],
       ),
     );
   }
 }
+
+
+
